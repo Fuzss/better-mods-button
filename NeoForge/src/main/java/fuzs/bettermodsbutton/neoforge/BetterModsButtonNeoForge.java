c3d5c2a011245f47c5139af2dd93ab21557b7177
Fuzss <28218241+Fuzss@users.noreply.github.com>
2025-12-10 23:43:42 +0100

    prepare 1.21.11 port

---
+++ NeoForge/src/main/java/fuzs/bettermodsbutton/neoforge/BetterModsButtonNeoForge.java
@@ -0,0 +1,42 @@
+package fuzs.bettermodsbutton.neoforge;
+
+import fuzs.bettermodsbutton.BetterModsButton;
+import fuzs.bettermodsbutton.neoforge.config.NeoForgeClientConfig;
+import net.minecraft.data.metadata.PackMetadataGenerator;
+import net.minecraft.network.chat.Component;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.config.ModConfig;
+import net.neoforged.fml.loading.FMLEnvironment;
+import net.neoforged.neoforge.data.event.GatherDataEvent;
+
+@Mod(BetterModsButton.MOD_ID)
+public class BetterModsButtonNeoForge {
+
+    public BetterModsButtonNeoForge(ModContainer modContainer) {
+        modContainer.registerConfig(ModConfig.Type.CLIENT, NeoForgeClientConfig.INSTANCE.getSpec());
+        registerLoadingHandlers(modContainer.getEventBus());
+    }
+
+    private static void registerLoadingHandlers(IEventBus eventBus) {
+        eventBus.addListener((final GatherDataEvent.Client event) -> {
+            event.getGenerator()
+                    .addProvider(true,
+                            PackMetadataGenerator.forFeaturePack(event.getGenerator().getPackOutput(),
+                                    Component.literal(event.getModContainer().getModInfo().getDescription())));
+        });
+    }
+
+    public static boolean isDevelopmentEnvironment() {
+        return !FMLEnvironment.isProduction();
+    }
+
+    public static boolean isDevelopmentEnvironment(String modId) {
+        if (!isDevelopmentEnvironment()) {
+            return false;
+        } else {
+            return Boolean.getBoolean(modId + ".isDevelopmentEnvironment");
+        }
+    }
+}
